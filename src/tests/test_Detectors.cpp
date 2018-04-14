11b77f045d35f23eeee6faf5d41e4275644e24e7
mechaZYK <me_zyk@foxmail.com>
2018-04-14 15:23:01 +0800

    1

--- src/tests/test_Detectors.cpp
+++ src/tests/test_Detectors.cpp
@@ -7,12 +7,16 @@ using namespace std;
 //std::string scene_file = "../../../datafile/pipe_scene.ply";
 ////std::string scene_file = "../../../datafile/pipes/5.ply";
 
-std::string surface_model_file = "../../../datafile/plat.ppfs";
-std::string scene_file = "../../../datafile/plat_scene.ply";
+//std::string surface_model_file = "../../../datafile/plat.ppfs";
+//std::string scene_file = "../../../datafile/plat_scene.ply";
 //std::string scene_file = "../../../datafile/plats/1.txt";
 
 //std::string surface_model_file = "../../../datafile/cylinder.ppfs";
 //std::string scene_file = "../../../datafile/cylinder_scene/5.ply";
+
+std::string surface_model_file = "pipe.ppfs";
+//std::string scene_file = "pipe_scene.ply";
+std::string scene_file = "pipes/1.txt";
 int main(int argc, char**argv) {
 	//CDetectModel3D dm;
 	//CDetectModel3D::trainOptions opt;
@@ -24,11 +28,11 @@ int main(int argc, char**argv) {
 	CDetectors3D detectors;
 	//set detect parameters
 	CDetectModel3D::detectObjParams params;
-	params.downSampleRatio = 0.06;
-	params.MaxOverlapDistRel = 0.5;
-	params.minScore = 0.3;
-	params.mlsOrder = 1;
-	params.param_1 = 5;
+  params.downSampleRatio = 0.06;
+  params.MaxOverlapDistRel = 0.5;
+  params.minScore = 0.1;
+  params.mlsOrder = 1;
+  params.param_1 = 5;
 	detectors.readScene(scene_file);
 	CDetectModel3D* model = detectors.readSurfaceModel(surface_model_file);
 	model->mDetectObjParams=params;
